44db02ebfb15671abdc9fdfda43f59edfd2fe365
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-01-18 18:43:03 +0600

    Refactor to addEventListener

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-function print(){
-  document.getElementById("print-click").innerHTML += "Button clicked!"+"<br>";
+
+// function print(){
+//   document.getElementById("print-click").innerHTML += "Button clicked!"+"<br>";
+// }
+// This needs onclick="print()" in button tag
+// Write a system that does the same without 'onclick="print()" in button tag'
+let inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function()){
+    document.getElementById("print-click").innerHTML += "Button clicked!"+"<br>";
 }
