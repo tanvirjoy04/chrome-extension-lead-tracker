d44feab5c4db25725dc91025d5bbb310529e3cdc
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-01-19 20:22:12 +0600

    Get leads from localStorage

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// Get the leads from the localStorage - PS: JSON.parse()                                                                                                                                                                                             
+// Store it in a variable, leadsFromLocalStorage
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads));  
+// Log out the variable
+console.log(leadsFromLocalStorage);
+
 inputBtn.addEventListener("click", function(){
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));  
   renderLeads();
+    
+    // To verify that it works:
+  console.log( localStorage.getItem("myLeads") )
+
 });
 
 function renderLeads(){
