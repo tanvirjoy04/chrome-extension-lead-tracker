407f36666c7756f8aeca51ee501fc2cee9a9b5a0
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-01-19 20:59:33 +0600

    Style the delete button

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-// Get the leads from the localStorage - PS: JSON.parse()                                                                                                                                                                                             
-// Store it in a variable, leadsFromLocalStorage
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads));  
-// Log out the variable
+
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 console.log(leadsFromLocalStorage);
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", function(){
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));  
   renderLeads();
-    
-    // To verify that it works:
-  console.log( localStorage.getItem("myLeads") )
-
 });
 
 function renderLeads(){
